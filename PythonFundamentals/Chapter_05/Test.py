e46231f0eecce9f81465e125f378790f84c591ca
dennis-k-aleksandrov <dennis.k.aleksandrov@gmail.com>
2021-03-10 16:07:33 +0100

    Chapter 5 code learnt

---
+++ PythonFundamentals/Chapter_05/Test.py
@@ -0,0 +1,7 @@
+def favorite_city(name):
+    print("One of my favorite cities is", name)
+
+
+favorite_city("Los Angeles, California")
+favorite_city("New York")
+favorite_city("Las Vegas, Nevada")
